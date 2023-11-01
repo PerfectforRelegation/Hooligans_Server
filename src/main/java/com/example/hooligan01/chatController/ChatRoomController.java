f4d24b39017f2ed6d58f5af1adc9c6b4526bba74
mudrhs1997 <mudrhs1997@daum.net>
2023-11-01 16:27:22 +0900

    feat: add chatroom user count

--- src/main/java/com/example/hooligan01/chatController/ChatRoomController.java
+++ src/main/java/com/example/hooligan01/chatController/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.example.hooligan01.chatController;
 
 import com.example.hooligan01.chatDto.ChatRoom;
+import com.example.hooligan01.dto.Message;
 import com.example.hooligan01.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +18,23 @@ public class ChatRoomController {
     // 채팅방 리스트
     @GetMapping("/room")
     public List<ChatRoom> rooms() {
-
         return chatRoomRepository.findAllRoom();
     }
 
     @GetMapping("/room/{roomId}")
     public ChatRoom chatRoom(@PathVariable String roomId) {
-
         return chatRoomRepository.findRoomById(roomId);
     }
 
     // 채팅방 생성
     @PostMapping("/room")
     public ChatRoom createRoom(@RequestBody ChatRoom chatRoom) {
-
         return chatRoomRepository.createChatRoom(chatRoom.getName());
+    }
+
+    // 채팅방 삭제
+    @DeleteMapping("/room/{roomId}")
+    public boolean deleteChatRoom(@PathVariable String roomId) {
+        return chatRoomRepository.deleteChatRoom(roomId);
     }
 }
--- src/main/java/com/example/hooligan01/repository/ChatRoomRepository.java
+++ src/main/java/com/example/hooligan01/repository/ChatRoomRepository.java
@@ -1,18 +1,23 @@
 package com.example.hooligan01.repository;
 
 import com.example.hooligan01.chatDto.ChatRoom;
+import com.example.hooligan01.dto.Message;
 import com.example.hooligan01.pubsub.RedisSubscriber;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -26,10 +31,14 @@ public class ChatRoomRepository {
 
     // Redis
     private static final String CHAT_ROOMS = "CHAT_ROOM";
+    public static final String USER_COUNT = "USER_COUNT";
+    public static final String ENTER_INFO = "ENTER_INFO";
     private final RedisTemplate<String, Object> redisTemplate;
 
-    //@Resource(name = "redisTemplate")
+//    @Resource(name = "redisTemplate")
     private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+    private HashOperations<String, String, String> hashOpsEnterInfo;
+    private ValueOperations<String, String> valueOps;
 
     // 채팅방의 대화 메시지를 발행하기 위한 redis topic 정보.
     // 서버별로 채팅방에 매치되는 topic 정보를 Map 에 넣어
@@ -54,14 +63,8 @@ public class ChatRoomRepository {
      * 채팅방 생성 : 서버간 채팅방 공유를 위해 redis hash 에 저장한다.
      */
     public ChatRoom createChatRoom(String name) {
-
         ChatRoom chatRoom = ChatRoom.create(name);
-
         opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
-
-//        Map<String, ChatRoom> d = opsHashChatRoom.entries(CHAT_ROOMS);
-//        System.out.println("d.get(chatRoom.getRoomId()) = " + d.get(chatRoom.getRoomId()));
-
         return chatRoom;
     }
 
@@ -69,13 +72,7 @@ public class ChatRoomRepository {
      * 채팅방 입장 : redis 에 topic 을 만들고
      * pub/sub 통신을 하기 위해 리스너를 설정한다.
      */
-    // 구아바 의존
-//    public boolean isNull(Object object) {
-//        return object == null;
-//    }
-
     public void enterChatRoom(String roomId) {
-
         ChannelTopic topic = topics.get(roomId);
 
         if (topic == null) {
@@ -83,6 +80,32 @@ public class ChatRoomRepository {
             redisMessageListener.addMessageListener(redisSubscriber, topic);
             topics.put(roomId, topic);
         }
+    }
+
+    public boolean deleteChatRoom(String name) {
+        opsHashChatRoom.delete(CHAT_ROOMS, name);
+        return true;
+    }
+
+    // 채팅방 유저수 조회
+    public long getUserCount(String roomId) {
+        return opsHashChatRoom.get(CHAT_ROOMS, roomId).getCount();
+    }
+
+    // 채팅방에 입장한 유저수 +1
+    public long plusUserCount(String roomId) {
+        ChatRoom chatRoom = opsHashChatRoom.get(CHAT_ROOMS, roomId);
+        chatRoom.plusCount();
+        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+        return opsHashChatRoom.get(CHAT_ROOMS, roomId).getCount();
+    }
+
+    // 채팅방에 입장한 유저수 -1
+    public long minusUserCount(String roomId) {
+        ChatRoom chatRoom = opsHashChatRoom.get(CHAT_ROOMS, roomId);
+        chatRoom.minusCount();
+        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+        return opsHashChatRoom.get(CHAT_ROOMS, roomId).getCount();
     }
 
     public ChannelTopic getTopic(String roomId) {
